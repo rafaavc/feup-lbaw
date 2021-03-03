2dbcd298db110b8b6a8a33853f6f8efcbedc3ab9
2dukes <ruipinto02@hotmail.com>
2021-03-03 17:17:02 +0000

    Started development of Sign In page

--- piu/src/components/inputIcon.php
+++ piu/src/components/inputIcon.php
@@ -1,20 +1,20 @@
 <?php
 
-    inputIconLeft('user'); echo "<br><br>";
-    inputIconLeft('envelope'); echo "<br><br>";
-    inputIconRight('lock'); echo "<br><br>";
-    inputIconRight('flag'); echo "<br><br>";
-    inputIconLeft('map-marker-alt'); echo "<br><br>";
+    // inputIconLeft('user'); echo "<br><br>";
+    // inputIconLeft('envelope'); echo "<br><br>";
+    // inputIconRight('lock'); echo "<br><br>";
+    // inputIconRight('flag'); echo "<br><br>";
+    // inputIconLeft('map-marker-alt'); echo "<br><br>";
 
     function inputIconRight($iconName) { ?>
-        <div class="d-inline-flex mb-3">
+        <div class="d-flex mb-3">
             <input type="text" class="form-control icon-right me-2" aria-label="Recipient's username" aria-describedby="basic-addon2">
             <?= getIcon($iconName, "right"); ?>
         </div>
     <?php } ?>
 
     <?php function inputIconLeft($iconName) { ?>
-        <div class="d-inline-flex mb-3">
+        <div class="d-flex mb-3">
             <?= getIcon($iconName, "left"); ?>
             <input type="text" class="form-control icon-left me-2" aria-label="Recipient's username" aria-describedby="basic-addon2">
         </div>
