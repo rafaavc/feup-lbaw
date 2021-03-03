c17bb9ed18ade258173fc7f11dced1b3be9e745b
Tiago Gomes <up201806658@fe.up.pt>
2021-03-03 17:16:34 +0000

    More progress in the post design

--- piu/src/components/post.php
+++ piu/src/components/post.php
@@ -10,7 +10,7 @@
         </div>
     </div>
     <div class="recipe-post-inner">
-        <div class="card mb-1 mx-3" style="max-width: 1000px;">
+        <div class="card mb-4 mx-3" style="max-width: 1000px;">
             <div class="row no-guttes" style="min-height: 300px;">
                 <div class="col-md-4 post-image">
                     <img src="../images/image1.png" class="bd-placeholder-img" width="100%" height="100%">
@@ -18,17 +18,33 @@
                 <div class="col-md-8">
                     <div class="card-body mt-3">
                         <h5 class="card-title">Post Title</h5>
-                        <p class="card-text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                        <p class="card-text post-description">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
                         <p><small class="text-muted">4.6 | ⭐⭐⭐⭐⭐ | 563 reviews</small></p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="row post-footer">
-        <div class="col-md-4">
-            
-        </div>
+    <div class="btn-group col-sm d-flex justify-content-center text-center border">
+        <button type="button" class="btn btn-light btn-lg post-button">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="red" class="bi bi-heart-fill mx-3" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+            </svg>
+            Add to Favourites
+        </button>
+        <button type="button" class="btn btn-light btn-lg post-button">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-info-circle mx-3" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+            </svg> 
+            See Post
+        </button>
+        <button type="button" class="btn btn-light btn-lg post-button">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-share mx-3" viewBox="0 0 16 16">
+                <path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5zm-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3z"/>
+            </svg>
+            Share
+        </button>
     </div>
 </div>
 
