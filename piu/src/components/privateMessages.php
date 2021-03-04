8e14882c51a9d03a333985de15a3801dd15b6ff3
rafaavc <rafavc2508@gmail.com>
2021-03-04 12:11:44 +0000

    Started private messages wireframe

---
+++ piu/src/components/privateMessages.php
@@ -0,0 +1,43 @@
+<div class="container" id="privateMessages">
+    <div class="row g-0">
+        <div class="col col-lg-4 p-3">
+            Start new
+        </div>
+        <div class="col p-3">
+            Conversation
+        </div>
+    </div>
+    <div class="row g-0">
+        <div class="col col-lg-4">
+            <div class="container p-3">
+                Ally
+            </div>
+            <hr class="dropdown-divider m-0">
+            <div class="container p-3">
+                Joe
+            </div>
+            <hr class="dropdown-divider m-0">
+            <div class="container p-3">
+                Sarah
+            </div>
+            <hr class="dropdown-divider m-0">
+            <div class="container p-3">
+                Wilson
+            </div>
+            <hr class="dropdown-divider m-0">
+            <div class="container p-3">
+                Caroline
+            </div>
+            <hr class="dropdown-divider m-0">
+        </div>
+        <div class="col p-3 position-relative">
+            <div class="row-6 g-0">
+                content...
+            </div>
+            <div class="form-floating" id="messageTextarea">
+                <textarea class="form-control" placeholder="Write your message..." id="floatingTextarea2" style="height: 5rem"></textarea>
+                <label for="floatingTextarea2">Message</label>
+            </div>
+        </div>
+    </div>
+</div>
