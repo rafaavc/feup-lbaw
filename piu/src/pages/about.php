f82c188e2ac3ec57fac56fabd0a77f0f59e50b4e
2dukes <ruipinto02@hotmail.com>
2021-03-04 22:59:45 +0000

    Started development of about us page

---
+++ piu/src/pages/about.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous" defer></script>
+    
+    <!-- Font Awesome -->
+    <link href="https://use.fontawesome.com/releases/v5.0.4/css/all.css" rel="stylesheet">
+    
+    <!-- Scripts -->
+    <link href="about.css" rel="stylesheet">
+
+    <title>About Us</title>
+</head>
+<body>
+    <h1 class="text-center my-5">About Us</h1>
+    <div class="shadow p-3 mb-5 bg-body rounded w-75 mx-auto">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        
+    </div>
+    <div class="text-center user-profiles w-75 mx-auto shadow-lg">
+        <p class="fs-4 text-start ps-4 pt-4">Our Team</p>
+        <div class="card-group p-5">
+            <div class="col-lg-2 col-md-6 py-2 px-sm-5 px-lg-1 mx-auto">
+                <div class="card border-0">
+                    <img src="../images/2dukes.jpeg" class="card-img-top rounded-circle" alt="...">
+                    <div class="card-body px-0">
+                        <p class="card-text">Rui Pinto</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-6 py-2 px-sm-5 px-lg-1 mx-auto">
+                <div class="card border-0">
+                    <img src="../images/rafaavc.jpeg" class="card-img-top rounded-circle" alt="...">
+                    <div class="card-body px-0">
+                        <p class="card-text">Rafael Cristino</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-6 py-2 px-sm-5 px-lg-1 mx-auto">
+                <div class="card border-0">
+                    <img src="../images/tiagoogomess.jpeg" class="card-img-top rounded-circle" alt="...">
+                    <div class="card-body px-0">
+                        <p class="card-text">Tiago Gomes</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-6 py-2 px-sm-5 px-lg-1 mx-auto">
+                <div class="card border-0">
+                    <img src="../images/a3brx.jpeg" class="card-img-top rounded-circle" alt="...">
+                    <div class="card-body px-0">
+                        <p class="card-text">Alexandre Abreu</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- <img src="../images/2dukes.jpeg" class="rounded-circle " alt="...">
+        <img src="../images/rafaavc.jpeg" class="rounded-circle me-5" alt="...">
+        <img src="../images/tiagoogomess.jpeg" class="rounded-circle me-5" alt="...">
+        <img src="../images/a3brx.jpeg" class="rounded-circle" alt="..."> -->
+    </div>
+</body>
+</html>
