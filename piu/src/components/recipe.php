19a1f24ae32f015d4d5f10a5774233f0c3e384a1
a3brx <a3brx@tuta.io>
2021-03-04 20:24:24 +0000

    added method

--- piu/src/components/recipe.php
+++ piu/src/components/recipe.php
@@ -1,35 +1,51 @@
 <?php
 
 $recipe = [
+    "name" => "Classic Tiramisu",
     "method" => [
-        "Stir the tofu" => "Preheat a large, heavy skillet over medium-high heat. Add 2 tablespoons oil. Break tofu apart over skillet into bite-size pieces, sprinkle with salt and pepper, then cook, stirring frequently with a thin metal spatula, until liquid cooks out and tofu browns, about 10 minutes. (If you notice liquid collecting in pan, increase heat to evaporate water.) Be sure to get under the tofu when you stir, scraping the bottom of the pan where the good, crispy stuff is and keeping it from sticking.",
+        "Step 1" => "Combine egg yolks and sugar in the top of a double boiler, over boiling water. Reduce heat to low, and cook for about 10 minutes, stirring constantly. Remove from heat and whip yolks until thick and lemon colored.",
+        "Step 2" => "Add mascarpone to whipped yolks. Beat until combined. In a separate bowl, whip cream to stiff peaks. Gently fold into yolk mixture and set aside.",
+        "Step 3" => "Split the lady fingers in half, and line the bottom and sides of a large glass bowl. Brush with coffee liqueur. Spoon half of the cream filling over the lady fingers. Repeat ladyfingers, coffee liqueur and filling layers. Garnish with cocoa and chocolate curls. Refrigerate several hours or overnight.",
+        "Step 4" => "To make the chocolate curls, use a vegetable peeler and run it down the edge of the chocolate bar."
     ]
 ];
 
-function printInstruction($number, $name, $text)
+function printInstruction($number, $name, $text, $image = null)
 { ?>
-    <h3 class="btn" data-bs-toggle="collapse" href="#instruction<?= $number ?>" role="button" aria-expanded="false" aria-controls="instruction<?= $number ?>">
-        <i class="fas fa-check-circle d-inline-block align-middle"></i>
-        <span class="d-inline-block align-middle"><?= $number ?>. <?= $name ?></span>
-    </h3>
-    <div class="collapse show" id="instruction<?= $number ?>">
-        <div class="card card-body">
-            <?= $text ?>
+    <section class="instruction d-inline-block col-12">
+        <h3 class="btn" data-bs-toggle="collapse" href="#instruction<?= $number ?>" role="button" aria-expanded="false" aria-controls="instruction<?= $number ?>">
+            <i class="fas fa-check-circle d-inline-block align-middle"></i>
+            <span class="d-inline-block align-middle"><?= $number ?>. <?= $name ?></span>
+        </h3>
+        <div class="collapse show" id="instruction<?= $number ?>">
+            <div class="d-flex">
+                <div class="col-<?= $image === null ? "12" : "8" ?> card card-body d-inline-block">
+                    <?= $text ?>
+                </div>
+                <?php if ($image !== null) { ?>
+                    <img class="col-3 d-inline-block" src="<?= $image ?>">
+                <?php } ?>
+            </div>
         </div>
-    </div>
+    </section>
 <?php }
 
 function printMethod($method)
 {
     $i = 1;
     foreach ($method as $name => $text) {
-        printInstruction($i++, $name, $text);
+        printInstruction(
+            $i++,
+            $name,
+            $text,
+            $i % 2 == 0 ? "https://www.thespruceeats.com/thmb/OCytFbckS2guE73MmUTAGLw6D9k=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/cubes-of-tofu-168621031-588670e23df78c2ccdef8c7d.jpg" : null
+        );
     }
 }
 
 ?>
 
-<h2>Method</h2>
-<section class="instruction">
+<div id="method" class="col-8">
+    <h2>Method</h2>
     <?php printMethod($recipe["method"]); ?>
-</section>
+</div>
