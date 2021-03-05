785352607f32b6e2846da488a0d615f043451326
Tiago Gomes <up201806658@fe.up.pt>
2021-03-05 19:03:51 +0000

    Added search page header

---
+++ piu/src/components/search.php
@@ -0,0 +1,26 @@
+<div class="container">
+    <div class="row search-header my-5 mx-1">
+        <div class="col info-text">
+            <span class="number-of-results">153</span>
+            results for
+            "<span class="searched-text">cozido à portuguesa</span>"
+        </div>
+        <div class="col sort-by">
+           <span class="sort-by-text">Sort by</span>
+
+           <span class="dropdown ms-2">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                    <span class="dropdown-title">Relevance</span>
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                    <li><a class="dropdown-item" href="#">Relevance</a></li>
+                    <li><a class="dropdown-item" href="#">Publication date</a></li>
+                    <li><a class="dropdown-item" href="#">Rating</a></li>
+                    <li><a class="dropdown-item" href="#">Difficulty</a></li>
+                    <li><a class="dropdown-item" href="#">Duration</a></li>
+                </ul>
+            </span>
+
+        </div>
+    </div>
+</div>
