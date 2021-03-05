8717005a10b23880ec857a2d8ebd8a8abf455414
Tiago Gomes <up201806658@fe.up.pt>
2021-03-05 10:29:22 +0000

    Recipe card for search results done

---
+++ piu/src/components/search_results_card.php
@@ -0,0 +1,12 @@
+
+<!-- Recipe card -->
+<div class="container">
+    <div class="card">
+        <div class="card-img-top"></div>
+        <div class="card-body">
+            <h4 class="card-title">Cozido à portuguesa</h4>
+            <p class="text-muted m-0">4.6 ⭐⭐⭐⭐⭐ | 563 reviews</p>
+            <p class="card-text mt-2">by <span class="username">Jamie Oliver</span></p>
+        </div>
+    </div>
+</div>
