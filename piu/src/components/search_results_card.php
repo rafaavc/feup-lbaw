81f8006133313fd9f88fdfb67006035ea55c074d
Tiago Gomes <up201806658@fe.up.pt>
2021-03-05 10:36:49 +0000

    Added group card

--- piu/src/components/search_results_card.php
+++ piu/src/components/search_results_card.php
@@ -10,3 +10,17 @@
         </div>
     </div>
 </div>
+
+<hr>
+
+<!-- Group card -->
+<div class="container">
+    <div class="card">
+        <div class="card-img-top"></div>
+        <div class="card-body">
+            <h4 class="card-title">Restaurante O Prego</h4>
+            <p class="text-muted m-0 group-info"><span class="group-info-number">237</span> recipes</p>
+            <p class="text-muted m-0 mt-2 group-info"><span class="group-info-number">57</span>  members</p>
+        </div>
+    </div>
+</div>
