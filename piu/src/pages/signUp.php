a8a709f782c7ee7802e7ec50acf5f8aa6b394b23
2dukes <ruipinto02@hotmail.com>
2021-03-06 12:05:29 +0000

    Started development of signUp page

--- piu/src/pages/signUp.php
+++ piu/src/pages/signUp.php
@@ -20,10 +20,11 @@
     <script src="../scripts/signIn.js" defer></script>
 
     <link href="signIn.css" rel="stylesheet">
+    <link href="signUp.css" rel="stylesheet">
     <link href="../components/inputIcon.css" rel="stylesheet">
     <link href="../components/footer.css" rel="stylesheet">
     <link href="../components/nav.css" rel="stylesheet">
-    <title>Sign In</title>
+    <title>Sign Up</title>
 
 </head>
 <body>
@@ -33,32 +34,34 @@
             <div class="row mt-3">
                 <div class="col-xl-6 sign-img">
                     <div class="d-grid gap-2 col-6 mx-auto mt-5 sign-left-text w-100 text-center">
-                        <div class="welcome-msg">
-                            <h1><strong>Welcome Back,</strong></h1>
-                            <h3>Sign in to continue</h3>
+                        <div class="welcome-msg text-start">
+                            <h1><strong>You're </strong>about to</h1>
+                            <h1 class="text-center">be part of this</h1>
+                            <h1 class="fw-bolder text-center">COMMUNITY</h1>
                             <button type="button" class="btn btn-dark w-100 mx-auto mt-4">Go Home</button>     
                         </div>
                     </div>
                 </div>
                 <div class="col-xl-6 sign-form mt-5">
-                    <h1>Sign In</h1>
+                    <h1>Sign Up</h1>
                     <h3>Please enter your account details.</h3>
                     
                     <?php
                         include_once "../components/inputIcon.php";
+                        echo "<span class='d-block mt-4'>Username</span>";
+                        inputIconLeft('user'); 
                         echo "<span class='d-block mt-4'>Email Address</span>";
                         inputIconLeft('envelope'); 
                         echo "<span class='d-block mt-4'>Password</span>";
                         inputIconLeft('lock'); 
+                        echo "<span class='d-block mt-4'>Repeat Password</span>";
+                        inputIconLeft('lock'); 
                     ?>
 
                     <div class="d-grid gap-2 col-6 mx-auto">
-                        <button type="button" class="btn btn-dark d-block">Sign In</button>     
+                        <button type="button" class="btn btn-dark d-block">Sign Up</button>     
                     </div>
-                    <span class="d-block text-center mt-3">Don't have an account? &nbsp;<a href="#" class="signUp-a">Sign Up</a></span>
-                    <div class="separator mt-3">or</div>
-                    
-                    <div class="g-signin2" data-width="200"></div>  
+                    <span class="d-block text-center mt-3">Already have an account? &nbsp;<a href="#" class="signUp-a">Sign In</a></span>
                 </div>
             </div>
         </div>
