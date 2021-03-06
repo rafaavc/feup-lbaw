8742934fb34fd4f31c06d541e714bec6eaa71312
Tiago Gomes <up201806658@fe.up.pt>
2021-03-06 00:26:44 +0000

    Moved search page from /components to /pages

--- piu/src/component.php
+++ piu/src/component.php
@@ -13,10 +13,6 @@ $component = isset($_GET['c']) ? $_GET['c'] : null;
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous" defer></script>
     <link href="/components/<?= $component ?>.css" rel="stylesheet">
 
-    <link href="/components/search_results_cards.css" rel="stylesheet">
-    <link href="/components/nav.css" rel="stylesheet">
-    <link href="/components/footer.css" rel="stylesheet">
-
     <!-- Font Awesome -->
     <link href="https://use.fontawesome.com/releases/v5.0.4/css/all.css" rel="stylesheet">
 
