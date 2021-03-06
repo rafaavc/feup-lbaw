9aecf7c9f45650ed43376ffb38965851013faa17
Tiago Gomes <up201806658@fe.up.pt>
2021-03-06 18:46:00 +0000

    Give rating done

---
+++ piu/src/scripts/rating.js
@@ -0,0 +1,18 @@
+
+let count;
+
+function starmark(item) {
+    count = item.id[0];
+    sessionStorage.starRating = count;
+    let subid = item.id.substring(1);
+    for(let i = 0; i < 5; i++) {
+        if(i < count)
+            document.getElementById((i+1)+subid).style.color="orange";
+        else
+            document.getElementById((i+1)+subid).style.color="black";
+    }
+}
+
+function result() {
+    alert("Rating: " + count);
+}
