487453768d55050ea896d5dfa3da8ad2fe5ffe64
rafaavc <rafavc2508@gmail.com>
2021-03-07 20:37:14 +0000

    Create Recipe Page done

--- piu/src/scripts/createRecipe.js
+++ piu/src/scripts/createRecipe.js
@@ -10,13 +10,15 @@ let counter = 1;
 
 const addStepButton = document.querySelector('#addStepButton');
 addStepButton.addEventListener('click', () => {
-    console.log('hello')
+
     const elem = document.createElement('template');
     elem.innerHTML = addStepButton.previousElementSibling.outerHTML;
+
     const header = document.createElement('h5');
     header.classList.add('mb-3');
     counter++;
     header.innerText = 'Step ' + counter;
+
     addStepButton.parentNode.insertBefore(header, addStepButton);
     addStepButton.parentNode.insertBefore(elem.content.firstElementChild, addStepButton);
 })
