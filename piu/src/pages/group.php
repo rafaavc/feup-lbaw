7ecbb429d69296e78596b2f2a3d4201e2f14789f
Tiago Gomes <up201806658@fe.up.pt>
2021-03-08 20:21:40 +0000

    Updates to the gorup page

--- piu/src/pages/group.php
+++ piu/src/pages/group.php
@@ -1,14 +1,44 @@
 <?php 
-include_once "../components/docHeader.php"
+
+$pageTitle = "Group Name | TasteBuds";
+$extraStyles = [ "../components/navPopups.css",
+    "../components/cover.css",
+    "../components/post.css",
+    "../components/membersFollowingBoxes.css",
+    "group.css"
+];
+$extraScripts = [
+    "../components/membersFollowingBoxes.js",
+    "../components/navPopups.js"
+];
+
+include_once "../components/docHeader.php";
+include_once "../components/nav.php";
+
+include_once "../components/post.php";
+
 ?>
 
-<?php include_once "../components/nav.php"; ?>
 <main>
-    <div class="container py-5">
-        <h1>Hello World!</h1>
+    <div class="container">
+        <div class="cover">
+            <?php include_once "../components/cover.php"; ?>
+        </div>
+        <div class="row group-body">
+            <div class="col members-box">
+                <?php include_once "../components/membersFollowingBoxes.php"; ?>
+            </div>
+            <div class="col posts-area">
+                <?php for ($i = 0; $i < 2; $i++) {
+                    displayRecipe(true);
+                } ?>
+            </div>
+        </div>
+        
+        
     </div>
 </main>
 <?php
     include_once "../components/footer.php";           
     include_once "../components/docFooter.php";
-?>         
+?>
