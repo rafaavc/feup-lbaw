22c5c09674b94235050233ac66b594876f75dddc
Tiago Gomes <up201806658@fe.up.pt>
2021-03-08 19:53:40 +0000

    Started group page

---
+++ piu/src/pages/group.php
@@ -0,0 +1,14 @@
+<?php 
+include_once "../components/docHeader.php"
+?>
+
+<?php include_once "../components/nav.php"; ?>
+<main>
+    <div class="container py-5">
+        <h1>Hello World!</h1>
+    </div>
+</main>
+<?php
+    include_once "../components/footer.php";           
+    include_once "../components/docFooter.php";
+?>         
