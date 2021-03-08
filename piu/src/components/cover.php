fe272039efabfe197373faff38570fb619796335
a3brx <a3brx@tuta.io>
2021-03-08 03:22:35 +0000

    start of cover

---
+++ piu/src/components/cover.php
@@ -0,0 +1,26 @@
+<header class="cover">
+    <img src="https://images-prod.healthline.com/hlcmsresource/images/AN_images/vegetarian-diet-plan-1296x728-feature.jpg" class="cover-image" alt="...">
+    <div class="container card">
+        <div class="row g-0 p-3">
+            <div class="image-container">
+                <img class="d-none d-md-inline-block rounded-circle" src="https://thispersondoesnotexist.com/image" alt="...">
+            </div>
+            <div class="col-9 card-body">
+                <h1 class="card-title">Alex Johnson</h1>
+                <p class="card-text">Sushi chef | Sushisan</p>
+                <table class="table table-borderless lh-1">
+                    <tr>
+                        <td>Recipes</td>
+                        <td>Followers</td>
+                        <td>Following</td>
+                    </tr>
+                    <tr>
+                        <td>1213</td>
+                        <td>102</td>
+                        <td>30</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+</header>
