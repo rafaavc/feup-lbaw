467bd7c84cd0b712bf86e1163671d4ae7552ff8b
a3brx <a3brx@tuta.io>
2021-03-08 10:39:14 +0000

    added follow and chat buttons

--- piu/src/components/cover.php
+++ piu/src/components/cover.php
@@ -2,10 +2,10 @@
     <img src="https://images-prod.healthline.com/hlcmsresource/images/AN_images/vegetarian-diet-plan-1296x728-feature.jpg" class="cover-image" alt="...">
     <div class="container card">
         <div class="row g-0 p-3">
-            <div class="image-container">
+            <div class="col-2 image-container">
                 <img class="d-none d-md-inline-block rounded-circle" src="https://thispersondoesnotexist.com/image" alt="...">
             </div>
-            <div class="col-9 card-body">
+            <div class="col-5 card-body">
                 <h1 class="card-title">Alex Johnson</h1>
                 <p class="card-text">Sushi chef | Sushisan</p>
                 <table class="table table-borderless lh-1">
@@ -20,6 +20,12 @@
                         <td>30</td>
                     </tr>
                 </table>
+            </div>
+            <div class="col-4 card-body text-end">
+                <div class="btn-group" role="group" aria-label="Basic outlined example">
+                    <button type="button" class="btn btn-outline-dark"><i class="fas fa-user-plus"></i> Follow</button>
+                    <button type="button" class="btn btn-outline-dark"><i class="fas fa-comments"></i> Chat</button>
+                </div>
             </div>
         </div>
     </div>
