dbac1a32625da74c94ddab15fc2170688d6e0c23
rafaavc <rafavc2508@gmail.com>
2021-03-08 16:25:53 +0000

    Added docHeader and docFooter; Created reports and users management pages

---
+++ piu/src/pages/reportsManagement.php
@@ -0,0 +1,15 @@
+<?php 
+    $pageTitle = "Reports Management";
+    include_once '../components/docHeader.php';
+    include_once "../components/nav.php"; 
+?>
+
+<div class="content-container">
+    <h1 class="mb-4">Reports Management</h1>
+    
+</div>
+
+<?php 
+    include_once "../components/footer.php";
+    include_once "../components/docFooter.php";  
+?>
