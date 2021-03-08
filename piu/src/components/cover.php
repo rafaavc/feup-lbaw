9b4e89618dcad03f402af41fb8d69ad33e82f54b
a3brx <a3brx@tuta.io>
2021-03-08 12:50:50 +0000

    added mobile version

--- piu/src/components/cover.php
+++ piu/src/components/cover.php
@@ -1,11 +1,11 @@
 <header class="cover">
     <img src="https://images-prod.healthline.com/hlcmsresource/images/AN_images/vegetarian-diet-plan-1296x728-feature.jpg" class="cover-image" alt="...">
     <div class="container card">
-        <div class="row g-0 p-3">
-            <div class="col-2 image-container">
-                <img class="d-none d-md-inline-block rounded-circle" src="https://thispersondoesnotexist.com/image" alt="...">
+        <div class="row g-0 p-3 text-center text-md-start">
+            <div class="col-md-2 image-container">
+                <img class="rounded-circle" src="https://thispersondoesnotexist.com/image" alt="...">
             </div>
-            <div class="col-6 card-body m-0">
+            <div class="col-md-6 card-body m-0">
                 <h1 class="card-title">Alex Johnson</h1>
                 <p class="card-text">Sushi chef | Sushisan</p>
                 <table class="table table-borderless lh-1">
@@ -21,7 +21,7 @@
                     </tr>
                 </table>
             </div>
-            <div class="col-4 card-body text-end m-0">
+            <div class="col-md-4 card-body text-md-end m-0">
                 <div class="btn-group" role="group" aria-label="">
                     <button type="button" class="btn btn-outline-dark"><i class="fas fa-user-plus"></i>Follow</button>
                     <button type="button" class="btn btn-outline-dark"><i class="fas fa-comments"></i>Chat</button>
@@ -29,7 +29,7 @@
             </div>
         </div>
         <div class="row">
-            <ul class="nav nav-tabs col-10 ps-3">
+            <ul class="nav nav-tabs col-md-10 ps-3">
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="#">Activity</a>
                 </li>
@@ -40,7 +40,7 @@
                     <a class="nav-link" href="#">Favourites</a>
                 </li>
             </ul>
-            <div class="col-2 text-center" style="margin-top: -1em;">
+            <div class="rating-box col-md-2 text-center d-none d-md-block">
                 <span class="small d-block">Average rating</span>
                 <div class="rating">
                     <span class="value me-1">3.3</span>
