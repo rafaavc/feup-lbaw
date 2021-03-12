c230fe874a048f7e29be4371bb7963149f2567bd
rafaavc <rafavc2508@gmail.com>
2021-03-12 20:34:20 +0000

    Added about us and faq anchors in index slider

--- piu/src/index.php
+++ piu/src/index.php
@@ -12,14 +12,20 @@
                 <div class="carousel-item home-slide content-general-padding active" style="background-image: url('images/image22.jpg')" data-bs-interval="2500">
                     <h2 class="display-4 mb-3 shadow"><strong>TasteBuds helps people build a community of</strong></h2>
                     <h2><div class="display-2 shadow-lg my-1"><strong>healthy</strong></div><div class="display-4 shadow"><strong>eating habits</strong></div></h2>
+                    <a role="button" class="btn btn-light shadow" href="<?=getRootUrl()?>/pages/about.php">About Us</a>
+                    <a role="button" class="btn btn-light me-4 shadow" href="<?=getRootUrl()?>/pages/faq.php">FAQ</a>
                 </div>
                 <div class="carousel-item home-slide content-general-padding" style="background-image: url('images/image21.jpg')" data-bs-interval="2500">
                     <h2 class="display-4 mb-3 shadow"><strong>TasteBuds helps people build a community of</strong></h2>
                     <h2><div class="display-2 shadow-lg my-1"><strong>diverse</strong></div><div class="display-4 shadow"><strong>eating habits</strong></div></h2>
+                    <a role="button" class="btn btn-light shadow" href="<?=getRootUrl()?>/pages/about.php">About Us</a>
+                    <a role="button" class="btn btn-light me-4 shadow" href="<?=getRootUrl()?>/pages/faq.php">FAQ</a>
                 </div>
                 <div class="carousel-item home-slide content-general-padding" style="background-image: url('images/image20.jpg'); background-position: 100% 70%" data-bs-interval="2500">
                     <h2 class="display-4 mb-3 shadow"><strong>TasteBuds helps people build a community of</strong></h2>
                     <h2><div class="display-2 shadow-lg my-1"><strong>tasty</strong></div><div class="display-4 shadow"><strong>eating habits</strong></div></h2>
+                    <a role="button" class="btn btn-light shadow" href="<?=getRootUrl()?>/pages/about.php">About Us</a>
+                    <a role="button" class="btn btn-light me-4 shadow" href="<?=getRootUrl()?>/pages/faq.php">FAQ</a>
                 </div>
             </div>
         </div>
