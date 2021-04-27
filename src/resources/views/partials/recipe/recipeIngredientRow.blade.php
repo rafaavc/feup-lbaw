4ea6e112b7bfacd7b9a9e077b0028b2edaba6887
2dukes <ruipinto02@hotmail.com>
2021-04-27 17:42:02 +0100

    Added quantity to editRecipe page

--- src/resources/views/partials/recipe/recipeIngredientRow.blade.php
+++ src/resources/views/partials/recipe/recipeIngredientRow.blade.php
@@ -3,7 +3,8 @@
         <div class="row g-3">
             <div class="col-sm">
                 <div class="form-floating">
-                    <input type="number" class="form-control" placeholder="0" aria-label="Quantity" id="quantityInput" <?=isset($ingredient->pivot->quantity) ? 'value="{{ $ingredient->pivot->quantity }}"' : ""?>>
+
+                    <input type="number" class="form-control" placeholder="0" aria-label="Quantity" id="quantityInput" value="{{ isset($ingredient->pivot->quantity) ? $ingredient->pivot->quantity : "" }}">
                     <label for="quantityInput">Quantity <span class='form-required'></span></label>
                 </div>
             </div>
