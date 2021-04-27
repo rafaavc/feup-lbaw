3acb52509e47f3510aae3fc8921d999d8cddc2cc
2dukes <ruipinto02@hotmail.com>
2021-04-27 22:47:19 +0100

    Created dropdown with search.

--- src/public/js/createRecipe.js
+++ src/public/js/createRecipe.js
@@ -22,3 +22,48 @@ addStepButton.addEventListener('click', () => {
     addStepButton.parentNode.insertBefore(header, addStepButton);
     addStepButton.parentNode.insertBefore(elem.content.firstElementChild, addStepButton);
 })
+
+const ingredientSelects = Array.from(document.querySelectorAll("select#ingredientSelect"));
+
+ingredientSelects.forEach(ingredientSelect => {
+    ingredientSelect.addEventListener('mousedown', (event) => {
+        event.preventDefault();
+        let target = event.target;
+        let searchDiv = target.closest("div.col-lg").nextElementSibling;
+        searchDiv.classList.toggle("show-searchBox");
+    });
+});
+
+const ingredientAnchors = Array.from(document.querySelectorAll("a.ingredient"));
+ingredientAnchors.forEach(ingredient => {
+    ingredient.addEventListener('click', ingredientSelected);
+});
+
+const searchBoxTexts = Array.from(document.querySelectorAll("input.searchBox-text"));
+searchBoxTexts.forEach(searchBox => {
+    searchBox.addEventListener('keyup', updateIngredients);
+});
+
+function updateIngredients(event) {
+    let target = event.target;
+    let filter = target.value.toUpperCase();
+    let searchBoxIngredients = target.parentElement.nextElementSibling;
+    let ingredients = searchBoxIngredients.getElementsByTagName("a");
+    for(let i = 0; i < ingredients.length; i++) {
+        let txtValue = ingredients[i].innerText;
+        if(txtValue.toUpperCase().indexOf(filter) > -1)
+            ingredients[i].style.display = "";
+        else
+            ingredients[i].style.display = "none";
+    }
+}
+
+function ingredientSelected(event) {
+    let target = event.target;
+    let parent = target.closest("div.search-div").previousElementSibling;
+    let ingredientOption = parent.querySelector("select.form-select option");
+    ingredientOption.value = target.value;
+    ingredientOption.innerHTML = target.innerHTML;
+    target.closest("div.search-div").classList.toggle("show-searchBox");
+    target.parentElement.previousElementSibling.firstElementChild.value = "";
+}
--- src/resources/views/partials/recipe/recipeIngredientRow.blade.php
+++ src/resources/views/partials/recipe/recipeIngredientRow.blade.php
@@ -25,8 +25,26 @@
     </div>
     <div class="col-lg">
         <div class="form-floating">
-            <input type="text" class="form-control" id="floatingInput" placeholder="Tomato" value="{{ isset($ingredient->name) ? $ingredient->name : "" }}">
+            <select class="form-select" id="ingredientSelect" aria-label="Quantity unit">
+                <option value="3">Tags</option>
+            </select>
+
+            {{-- <input type="text" class="form-control" id="floatingInput" placeholder="Tomato" value="{{ isset($ingredient->name) ? $ingredient->name : "" }}"> --}}
             <label for="floatingInput">Name <span class='form-required'></span></label>
+        </div>
+    </div>
+    <div class="search-div collapse navbar-collapse justify-content-center flex-grow-1 normalize mt-0" id="navbarSearch" data-bs-parent="#navbarContainer">
+        <div class="d-flex">
+            <input type="text" class="searchBox-text form-control rounded-0" placeholder="Search..." aria-label="Recipient's username" aria-describedby="basic-addon2">
+        </div>
+        <div class="searchBox-ingredient">
+            <a class="list-group-item list-group-item-action ingredient">About</a>
+            <a class="list-group-item list-group-item-action ingredient">Base</a>
+            <a class="list-group-item list-group-item-action ingredient">Blog</a>
+            <a class="list-group-item list-group-item-action ingredient">Contact</a>
+            <a class="list-group-item list-group-item-action ingredient">Custom</a>
+            <a class="list-group-item list-group-item-action ingredient">Support</a>
+            <a class="list-group-item list-group-item-action ingredient">Tools</a>
         </div>
     </div>
 </div>
