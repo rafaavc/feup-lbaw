4e1506a9dc4fde97715332ba40a9783c019b1352
a3brx <a3brx@tuta.io>
2021-05-06 14:04:27 +0100

    special cases for users who do not have recipes, reviews or favourites

--- src/resources/views/pages/user/reviews.blade.php
+++ src/resources/views/pages/user/reviews.blade.php
@@ -5,9 +5,13 @@
 @section('body')
     <div class="row first-recipe-mt">
         <h3>Reviews</h3>
-        @foreach($reviews as $comment)
-            @include('partials.preview.comment')
-        @endforeach
+        @if(sizeof($reviews) > 0)
+            @foreach($reviews as $comment)
+                @include('partials.preview.comment')
+            @endforeach
+        @else
+            <p>This user has not made any reviews.</p>
+        @endif
     </div>
     <!--<div class="row">
        <button type="button" class="btn btn-dark load-more w-25 mt-5 mx-auto">Load More</button>
