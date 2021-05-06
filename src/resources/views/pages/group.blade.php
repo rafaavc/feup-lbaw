9befb4ff2e8a679d9205165ff418393f2ffc8988
a3brx <a3brx@tuta.io>
2021-05-06 13:46:56 +0100

    added requests to group page

--- src/resources/views/pages/group.blade.php
+++ src/resources/views/pages/group.blade.php
@@ -6,7 +6,7 @@
     <link href="{{ asset('css/navPopups.css') }}" rel="stylesheet"/>
     <link href="{{ asset('css/profile_cover.css') }}" rel="stylesheet"/>
     <link href="{{ asset('css/post.css') }}" rel="stylesheet"/>
-    <link href="{{ asset('css/membersFollowingBoxes.css') }}" rel="stylesheet"/>
+    <link href="{{ asset('css/components/membersFollowingBoxes.css') }}" rel="stylesheet"/>
     <link href="{{ asset('css/breadcrumb.css') }}" rel="stylesheet"/>
     <link href="{{ asset('css/group.css') }}" rel="stylesheet"/>
 @endpush
@@ -44,6 +44,9 @@
             <div class="row group-body">
                 <div class="col-md-4 p-0 pe-md-4 mt-5">
                     @include('partials.profile.peopleBox', ['name' => 'Members', 'people' => $group->members])
+                    @include('partials.profile.requestBox', ['name' => 'Member Requests',
+                                                             'requests' => $group->requests,
+                                                             'new' => true])
                 </div>
 
                 <div class="col-md-8 posts-area ps-md-4 mt-5">
