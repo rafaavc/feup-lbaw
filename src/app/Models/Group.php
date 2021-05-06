06b88edae1b203596c089a823ebe03b695190082
a3brx <a3brx@tuta.io>
2021-05-06 11:50:52 +0100

    added group relations

--- src/app/Models/Group.php
+++ src/app/Models/Group.php
@@ -2,10 +2,34 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Member;
+use App\Models\Recipe;
 use Illuminate\Database\Eloquent\Model;
 
 class Group extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'name', 'description', 'visibility'
+    ];
+
+    public function moderators()
+    {
+        return $this->belongsToMany(Member::class, 'tb_group_moderator', 'id_group', 'id_member');
+    }
+
+    public function members()
+    {
+        return $this->belongsToMany(Member::class, 'tb_group_member', 'id_group', 'id_member');
+    }
+
+    public function requests()
+    {
+        return $this->belongsToMany(Member::class, 'tb_group_request', 'id_group', 'id_member')
+            ->withPivot("state", "timestamp");
+    }
+
+    public function recipes()
+    {
+        return $this->hasMany(Recipe::class, 'id_group', 'id');
+    }
 }
