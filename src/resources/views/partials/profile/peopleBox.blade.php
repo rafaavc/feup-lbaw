86e55ee494892496f71a60414fbc4b6a62d57a9f
a3brx <a3brx@tuta.io>
2021-05-06 14:00:04 +0100

    added margin to people box

--- src/resources/views/partials/profile/peopleBox.blade.php
+++ src/resources/views/partials/profile/peopleBox.blade.php
@@ -1,4 +1,4 @@
-<div class="card shadow-sm people-box">
+<div class="card shadow-sm people-box mt-4">
     <div class="card-body">
         <h5 class="card-title mb-4">{{$name}}</h5>
         <div class="row g-5 mb-5">
