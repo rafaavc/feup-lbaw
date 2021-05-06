40b49b295b9d28d09a0fce44b1c4759259098db6
a3brx <a3brx@tuta.io>
2021-05-06 11:42:54 +0100

    created controller and model for group, added controller functions

---
+++ src/app/Http/Controllers/GroupController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class GroupController extends Controller
+{
+    // ----------------------------------------------------------------
+    // API
+    // ----------------------------------------------------------------
+
+    public function post(Request $request)
+    {
+
+    }
+
+    public function put(Request $request, Group $group)
+    {
+
+    }
+
+    public function remove(Group $group)
+    {
+
+    }
+
+    public function get(Group $group)
+    {
+
+    }
+
+    public function request(Group $group)
+    {
+
+    }
+
+    public function accept(Group $group, Member $user)
+    {
+
+    }
+
+    public function decline(Group $group, Member $user)
+    {
+
+    }
+
+    public function addModerator(Group $group, Member $user)
+    {
+
+    }
+
+    public function removeMember(Group $group, Member $user)
+    {
+
+    }
+
+    public function getMembers(Group $group)
+    {
+
+    }
+
+    // ----------------------------------------------------------------
+    // Pages
+    // ----------------------------------------------------------------
+
+    public function create()
+    {
+
+    }
+
+    public function createAction(Request $request)
+    {
+
+    }
+
+    public function update(Group $group)
+    {
+
+    }
+
+    public function updateAction(Request $request, Group $group)
+    {
+
+    }
+
+    public function deleteAction(Group $group)
+    {
+
+    }
+
+    public function view(Group $group)
+    {
+
+    }
+}
