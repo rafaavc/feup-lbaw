5000d1212c6b733204fc79ee444d661bbfefd0c8
2dukes <ruipinto02@hotmail.com>
2021-05-07 11:23:36 +0100

    Users FTS.

--- src/app/Http/Controllers/SearchController.php
+++ src/app/Http/Controllers/SearchController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Member;
+use Illuminate\Foundation\Auth\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
@@ -26,22 +28,44 @@ class SearchController extends Controller
      */
     public function show(Request $request)
     {
-        // Recipes
         $searchStr = preg_replace("/\s+/", " | ", $request->input('searchQuery'));
 
+        // Recipes
         $recipes = DB::table('recipes_fts_view')
             ->selectRaw('*, search, ts_rank(search, to_tsquery(\'english\', ?)) AS rank', [$searchStr])
             ->when($searchStr, function($query, $searchStr) {
-                return $query
-                    ->whereRaw('search @@ to_tsquery(\'english\', ?) AND member_id <> ? AND recipe_visibility(recipe_id, ?) = TRUE', [$searchStr, (Auth::check()) ? Auth::id() : 0, Auth::id()])
-                    ->orderByDesc('rank');
+                if(Auth::guard('admin')->check()) {
+                    return $query
+                        ->whereRaw('search @@ to_tsquery(\'english\', ?)', [$searchStr])
+                        ->orderByDesc('rank');
+                } else {
+                    return $query
+                        ->whereRaw('search @@ to_tsquery(\'english\', ?) AND member_id <> ? AND recipe_visibility(recipe_id, ?) = TRUE', [$searchStr, (Auth::check()) ? Auth::id() : 0, Auth::id()])
+                        ->orderByDesc('rank');
+                }
             }, function ($query) {
                 return $query
                     ->inRandomOrder()
                     ->limit(20);
             })
             ->get();
-        return $recipes;
-        // return response()->json(['message' => $request->query()]);
+
+        // Users
+        $users = Member::selectRaw('*, search, ts_rank(search, to_tsquery(\'english\', ?)) AS rank', [$searchStr])
+            ->when($searchStr, function($query, $searchStr) {
+                return $query
+                    ->whereRaw('search @@ to_tsquery(\'english\', ?) AND member_id <> ?', [$searchStr, (Auth::check()) ? Auth::id() : 0])
+                    ->orderByDesc('rank');
+            }, function($query) {
+                return $query
+                    ->inRandomOrder()
+                    ->limit(20);
+            })
+            ->get();
+
+        return response()->json([
+            'recipes' => $recipes,
+            'users' => $users
+        ]);
     }
 }
