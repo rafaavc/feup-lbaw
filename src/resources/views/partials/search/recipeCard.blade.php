4241a43dcc8f2db615d0e6409e9287aa5ee772d9
2dukes <ruipinto02@hotmail.com>
2021-05-07 17:01:51 +0100

    Further development.

--- src/resources/views/partials/search/recipeCard.blade.php
+++ src/resources/views/partials/search/recipeCard.blade.php
@@ -2,10 +2,17 @@
     <a type="button" href="{{ url('/recipe/' . $recipe->recipe_id) }}" class="btn card shadow-sm p-2 mx-auto h-100">
         <img class="card-img-top" src="{{ asset('storage/images/recipes/' . $recipe->recipe_id . '/1.jpg') }}">
         <h5 class="card-title">{{ $recipe->recipe_name }}</h5>
+        @php
+            $score = \App\Models\Recipe::find($recipe->recipe_id)->score;
+        @endphp
         <div class="card-body w-100">
-            <p class="text-muted m-0" style="font-size: .8rem">{{ \App\Models\Recipe::find($recipe->recipe_id)->score }}
-                @for ($i = 1; $i <= floor(\App\Models\Recipe::find($recipe->recipe_id)->score); $i++)
-                    <i class="fas fa-star active"></i>
+            <p class="text-muted m-0" style="font-size: .8rem">{{ round($score, 1) }}
+                @for ($i = 1; $i <= 5; $i++)
+                    @if($i <= floor($score))
+                        <i class="fas fa-star active"></i>
+                    @else
+                        <i class="fas fa-star"></i>
+                    @endif
                 @endfor
                 | {{ \App\Models\Recipe::find($recipe->recipe_id)->getNumReviews() }} reviews</p>
             <p class="card-text mt-2">by {{ $recipe->member_name }}</p>
