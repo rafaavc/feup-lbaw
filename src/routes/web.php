b547a348dea3ef2ec2ac81ed9945eb63f40095f7
a3brx <a3brx@tuta.io>
2021-05-18 17:42:31 +0100

    category middleware

--- src/routes/web.php
+++ src/routes/web.php
@@ -62,7 +62,7 @@ Route::post('group', 'GroupController@createAction')->middleware('can:create,App
 // ----------------------------------------------------------------
 // List pages
 // ----------------------------------------------------------------
-Route::get('category/{category}', 'CategoryController@view')->middleware('can:view,category');
+Route::get('category/{category}', 'CategoryController@view');
 Route::get('feed', 'FeedController@view');
 Route::get('search', 'SearchController@view');
 
