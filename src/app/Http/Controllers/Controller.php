81d4aef995eb364351560b745bb53e89bc021d79
2dukes <ruipinto02@hotmail.com>
2021-05-18 23:36:35 +0100

    Small fix.

--- src/app/Http/Controllers/Controller.php
+++ src/app/Http/Controllers/Controller.php
@@ -12,6 +12,6 @@ class Controller extends BaseController
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     public function __construct() {
-        $this->middleware('CheckBanned', ['except' => ['login', 'logout']]);
+        $this->middleware('CheckBanned', ['except' => ['logout']]);
     }
 }
