d130f32d6206e57f19afbc3b49de5a4af47e070f
Tiago Gomes <up201806658@fe.up.pt>
2021-06-03 10:26:26 +0100

    Small bug fix

--- src/resources/views/partials/search/groupCard.blade.php
+++ src/resources/views/partials/search/groupCard.blade.php
@@ -7,8 +7,8 @@
         @endif
         <div class="card-body">
             <h4 class="card-title mt-3">{{ $group->name }}</h4>
-            <p class="text-muted m-0 card-info"><span class="info-number">{{ $group->members->count() }}</span> recipes</p>
-            <p class="text-muted m-0 mt-1 card-info"><span class="info-number">{{ $group->recipes->count() }}</span>  members</p>
+            <p class="text-muted m-0 card-info"><span class="info-number">{{ $group->recipes->count() }}</span> recipes</p>
+            <p class="text-muted m-0 mt-1 card-info"><span class="info-number">{{ $group->members->count() }}</span>  members</p>
         </div>
     </a>
 </div>
