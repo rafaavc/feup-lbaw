15dcc1d429cded54a9d263b80f68ab81757c69d0
rafaavc <rafavc2508@gmail.com>
2021-06-04 02:49:25 +0100

    Improving following boxes and group boxes

--- src/resources/views/partials/profile/peopleBox.blade.php
+++ src/resources/views/partials/profile/peopleBox.blade.php
@@ -1,10 +1,14 @@
 <div id="peopleBox" class="card shadow-sm people-box mt-4">
     <div class="card-body">
         <h5 class="card-title mb-4">{!! $name !!}</h5>
-        <div class="row g-5 mb-5">
-            @foreach($people as $person)
-                @include('partials.profile.peopleBoxEntry')
-            @endforeach
+        <div class="row g-5 {{ isset($actions) ? "mb-5" : "mb-2" }}">
+            @if (sizeof($people) == 0)
+                <p class="mb-0">{{ $errorMessage ?? "No one was found." }}</p>
+            @else
+                @foreach($people as $person)
+                    @include('partials.profile.peopleBoxEntry')
+                @endforeach
+            @endif
         </div>
         @if(isset($actions))
             {!! $actions !!}
