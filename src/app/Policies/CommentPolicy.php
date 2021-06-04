86dd6d355a3067ce820af0ce0c6539e0caeee82c
a3brx <a3brx@tuta.io>
2021-06-04 17:36:44 +0100

    added policy comments

--- src/app/Policies/CommentPolicy.php
+++ src/app/Policies/CommentPolicy.php
@@ -11,11 +11,23 @@ class CommentPolicy
 {
     use HandlesAuthorization;
 
+    /**
+     * All the comments are available in their recipes
+     *
+     * @param Member|null $member
+     * @return bool
+     */
     public function view(?Member $member)
     {
         return true;
     }
 
+    /**
+     * All users can create a comment
+     *
+     * @param Member|null $member
+     * @return bool
+     */
     public function create(?Member $member)
     {
         if (Auth::guard('admin')->check())
@@ -25,6 +37,13 @@ class CommentPolicy
         return true;
     }
 
+    /**
+     * Only the author of the comment can edit it
+     *
+     * @param Member|null $member
+     * @param Comment $comment
+     * @return bool
+     */
     public function update(?Member $member, Comment $comment)
     {
         if (Auth::guard('admin')->check())
@@ -34,6 +53,13 @@ class CommentPolicy
         return $member->id == $comment->owner->id;
     }
 
+    /**
+     * Only the author of the comment or an admin can delete it
+     *
+     * @param Member|null $member
+     * @param Comment $comment
+     * @return bool
+     */
     public function delete(?Member $member, Comment $comment)
     {
         if (Auth::guard('admin')->check())
