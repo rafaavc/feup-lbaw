ffd1afd06014020e6854e36b8e0dafaca9a72785
Tiago Gomes <up201806658@fe.up.pt>
2021-06-04 08:37:24 +0100

    Added print button functionality

--- src/resources/views/pages/recipe.blade.php
+++ src/resources/views/pages/recipe.blade.php
@@ -81,7 +81,7 @@
                         </li>
                     @endif
                     <li class="list-group-item">
-                        <a href="#" data-toggle="tooltip" data-placement="top" title="Generate a PDF of the recipe page">
+                        <a href="#" onclick="window.print();return false;" data-toggle="tooltip" data-placement="top" title="Generate a PDF of the recipe page">
                             <span class="legend">Print</span><i class="fas fa-print"></i>
                         </a>
                     </li>
