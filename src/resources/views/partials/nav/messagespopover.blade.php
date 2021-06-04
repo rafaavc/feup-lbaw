ea418f2c313fbf773b47598ae863bfddbd798f01
a3brx <a3brx@tuta.io>
2021-06-04 16:54:12 +0100

    message link

--- src/resources/views/partials/nav/messagespopover.blade.php
+++ src/resources/views/partials/nav/messagespopover.blade.php
@@ -1,9 +1,8 @@
 <li class="nav-item">
-    <a href="{{ url('/chat') }}">
-        <button class="btn btn-primary btn-sm mt-2 nav-popover position-relative d-none d-lg-block">
-            <i class="fas fa-envelope"></i>
-        </button>
-    </a>
+    <button class="btn btn-primary btn-sm mt-2 nav-popover position-relative d-none d-lg-block"
+            onclick="window.location='{{ url('/chat') }}'">
+        <i class="fas fa-envelope"></i>
+    </button>
     <a class="nav-link d-inline-block d-lg-none" href="{{ url('/chat') }}">
         @include('partials.nav.icontext', ['icon' => 'envelope', 'text' => 'Messages'])
     </a>
